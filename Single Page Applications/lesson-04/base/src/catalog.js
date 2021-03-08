45cbc31b2168370b0669ca65f08dbf02188c76f7
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-03-08 15:15:11 +0200

    update Cookbook

--- Single Page Applications/lesson-04/base/src/catalog.js
+++ Single Page Applications/lesson-04/base/src/catalog.js
@@ -1,62 +1,36 @@
 import { e } from './dom.js'
+import { showDetails } from './details.js'
 
-export async function getRecipes() {
+async function getRecipes() {
     const response = await fetch('http://localhost:3030/data/recipes');
     const recipes = await response.json();
 
     return recipes;
 }
 
-async function getRecipeById(id) {
-    const response = await fetch('http://localhost:3030/data/recipes/' + id);
-    const recipe = await response.json();
-
-    return recipe;
-}
-
 export function createRecipePreview(recipe) {
-    const result = e('article', { className: 'preview', onClick: toggleCard },
+    const result = e('article', { className: 'preview', onClick: () => showDetails(recipe._id) },
         e('div', { className: 'title' }, e('h2', {}, recipe.name)),
         e('div', { className: 'small' }, e('img', { src: recipe.img })),
     );
 
     return result;
 
-    async function toggleCard() {
-        const fullRecipe = await getRecipeById(recipe._id);
-
-        result.replaceWith(createRecipeCard(fullRecipe));
-    }
-}
-
-function createRecipeCard(recipe) {
-    const result = e('article', {},
-        e('h2', {}, recipe.name),
-        e('div', { className: 'band' },
-            e('div', { className: 'thumb' }, e('img', { src: recipe.img })),
-            e('div', { className: 'ingredients' },
-                e('h3', {}, 'Ingredients:'),
-                e('ul', {}, recipe.ingredients.map(i => e('li', {}, i))),
-            )
-        ),
-        e('div', { className: 'description' },
-            e('h3', {}, 'Preparation:'),
-            recipe.steps.map(s => e('p', {}, s))
-        ),
-    );
-
-    return result;
 }
 
 let main;
 let section;
+let setActivNav;
 
-export function setUpCatalog(mainTarget, sectionTarget) {
+export function setUpCatalog(mainTarget, sectionTarget, setActiveNavCB) {
     main = mainTarget;
     section = sectionTarget;
+    setActivNav = setActiveNavCB
 }
 
 export async function showCatalog() {
+    setActivNav('catalogLink');
+
     section.innerHTML = '<p style="color: white">Loading...</p>';
     main.innerHTML = '';
     main.appendChild(section);
@@ -68,5 +42,5 @@ export async function showCatalog() {
     let fragment = document.createDocumentFragment();
     cards.forEach(c => fragment.appendChild(c));
     section.appendChild(fragment);
-    
+
 }
