f3b8d13b7111666f3f996a72fab3a7c04bb4825c
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-03-12 22:48:34 +0200

    update SoftForum

--- Exercise Single Page Applications/01.Forum/comments.js
+++ Exercise Single Page Applications/01.Forum/comments.js
@@ -1,88 +1,88 @@
-export async function getCommentByPost(id) {
-
-    let request = await fetch(`http://localhost:3030/jsonstore/collections/myboard/posts/${id}`)
-
-    if (request.ok) {
-        let data = await request.json();
-        return data
-    } else {
-        alert("Something went wrong!");
-    }
-}
-
 let main;
 let section;
 
-export function setupContent(mainTarget, sectionTarget) {
+export function setupComment(mainTarget, sectionTarget) {
     main = mainTarget;
     section = sectionTarget;
+
+    section.querySelector('button').addEventListener('click', setComment)
 }
 
-export async function showContent(id) {
-    main.innerHTML = '';
+export async function showComment(id) {
+    let post = await getPostById(id)
+    let postInfo = createPostInfo(post)
 
-    let form = section.querySelector('form');
-
-    form.addEventListener('submit', async (ev) => {
-        ev.preventDefault();
-        let formData = new FormData(ev.target);
-
-        let commentContent = formData.get('commentText')
-        let username = formData.get('username')
-
-        if (commentContent != '' || username != '') {
-
-            let response = await fetch('http://localhost:3030/jsonstore/collections/myboard/comments', {
-                method: "post",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ commentContent, username }),
-
-            });
-
-            let data = await response.json()
-console.log(data);
-            let comment = document.createElement('div')
-            comment.className = 'comment'
-            comment.innerHTML = `
-                <header class="header">
-                    <p><span>${data.username}</span> posted on <time>2020-10-10 12:08:28</time></p>
-                </header>
-                <div class="comment-main">
-                    <div class="userdetails">
-                        <img src="./static/profile.png" alt="avatar">
-                                </div>
-                                <div class="post-content">
-                                    <p>${data.commentContent}</p>
-                                </div>
-                            </div>
-                            <div class="footer">
-                                <p><span>5</span> likes</p>
-                            </div>
-                        </div>
-                        <div id="contentCommnets">
-                        </div>
-                        
-                        <div class="answer-comment">
-                            <p><span>currentUser</span> comment:</p>
-                            <div class="answer">
-                                <form>
-                                    <textarea name="commentText" id="comment" cols="30" rows="10"></textarea>
-                                    <div>
-                                        <label for="username">Username <span class="red">*</span></label>
-                                        <input type="text" name="username" id="username">
-                                    </div>
-                                    <button>Post</button>
-                                </form>
-                            </div>
-                        </div>`
-            section.innerHTML = ''
-            section.prepend(comment)
-
-            ev.target.reset()
-        }
-    })
-
+    main.innerHTML = ''
+    main.appendChild(postInfo)
     main.appendChild(section)
+
 }
+
+async function setComment(ev) {
+    ev.preventDefault()
+
+    let username = document.getElementById('username').value
+    let comment = document.getElementById('comment').value
+
+    if (username === '' || comment === '') {
+        return alert('All fields are required!');
+    }
+
+    let response = await fetch('http://localhost:3030/jsonstore/collections/myboard/comments', {
+        method: 'post',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({ username, comment })
+    });
+
+    createCommentsCard(await response.json())
+
+    document.getElementById('username').value = ''
+    document.getElementById('comment').value = ''
+}
+
+function createCommentsCard(data) {
+    let comment = document.createElement('div')
+    comment.className = 'comment'
+    comment.innerHTML = `
+        <header class="header">
+            <p><span>${data.username}</span></p>
+        </header >
+        <div class="comment-main">
+            <div class="userdetails">
+                <img src="./static/profile.png" alt="avatar">
+            </div>
+                <div class="post-content">
+                    <p>${data.comment}</p>
+                </div>
+        </div>
+        <div class="footer">
+        </div>`
+    section.prepend(comment)
+    return section
+
+}
+
+async function getPostById(id) {
+    let url = `http://localhost:3030/jsonstore/collections/myboard/posts/${id}`;
+    let request = await fetch(url);
+    let data = await request.json();
+
+    return data;
+}
+
+function createPostInfo(data) {
+    let element = document.createElement('div')
+    element.className = 'theme-content'
+    element.innerHTML = `
+    <div class="theme-title">
+        <div class="theme-name-wrapper">
+            <div class="theme-name">
+                <h2>${data.topic}</h2>
+                <p>Date: <time>${data.date}</time></p>
+            </div>
+        </div>
+    </div>`
+    return element
+}
+
+
