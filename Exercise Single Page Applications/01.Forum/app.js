5e630db31cf384664199f6e69019b22b3b0fc8dd
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-03-12 22:47:51 +0200

    update SoftForum

--- Exercise Single Page Applications/01.Forum/app.js
+++ Exercise Single Page Applications/01.Forum/app.js
@@ -12,7 +12,55 @@ setupNavigation()
 
 showHome()
 let links = {
+    'homeLink': showHome,import { setupHome, showHome } from './home.js';
+import { setupPost, showPost } from './posts.js';
+import { setupComment, showComment } from './comments.js';
+
+let main = document.querySelector('main');
+
+setupSection('home-page', setupHome);
+setupSection('topic-box', setupPost);
+setupSection('theme-content', setupComment);
+setupNavigation()
+
+
+showHome()
+let links = {
     'homeLink': showHome,
+    'topicLink': showPost,
+    'public': showComment,
+}
+
+
+function setupSection(sectionId, setup) {
+    let section = document.getElementById(sectionId)
+    setup(main, section)
+}
+
+function setupNavigation() {
+    // document.querySelector('#topicLink').addEventListener('click', (ev) => {
+    //     ev.preventDefault();
+
+    //     let view = links[ev.target.id];
+    //     if (typeof view == 'function') {
+    //         view();
+    //     }
+
+    // })
+
+    document.querySelector('#homeLink').addEventListener('click', (ev) => {
+        ev.preventDefault();
+
+        let view = links[ev.target.id];
+        if (typeof view == 'function') {
+            view();
+        }
+
+    })
+
+}
+
+
     'topicLink': showPost,
     'public': showContent,
 }
