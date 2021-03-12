c0e982085cba80cb99866b96f04cc201b779feb7
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-03-12 22:49:14 +0200

    update SoftForum

--- Exercise Single Page Applications/01.Forum/home.js
+++ Exercise Single Page Applications/01.Forum/home.js
@@ -1,16 +1,4 @@
-import { getCommentByPost, showContent } from './comments.js'
-
-document.querySelector('#home').addEventListener('click', (ev) => {
-    if (ev.target.tagName == 'H2') {
-        ev.preventDefault();
-        let parent = ev.target.parentNode;
-        while (parent.className != 'topic-container') {
-            parent = parent.parentNode;
-        }
-        let id = parent.id;
-        showContent(id)
-    }
-});
+import { showComment } from './comments.js'
 
 let main;
 let section
@@ -18,15 +6,30 @@ let section
 export function setupHome(mainTarget, sectionTarget) {
     main = mainTarget;
     section = sectionTarget;
+
+    section.addEventListener('click', (ev) => {
+        if (ev.target.tagName == 'H2') {
+            ev.preventDefault();
+            let parent = ev.target.parentNode;
+            while (parent.className != 'topic-container') {
+                parent = parent.parentNode;
+            }
+            let id = parent.id;
+            showComment(id)
+        }
+    });
 }
 
-export function apend(apendSection) {
-    section.appendChild(apendSection)
-    showHome()
-}
 
 export function showHome() {
     main.innerHTML = '';
     main.appendChild(section)
 
 }
+
+export function apend(apendSection) {
+    document.querySelector('.topic-title').appendChild(apendSection)
+    showHome()
+}
+
+
