371d64a6d80573892eeea5cc6483ea50bde8b75e
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-03-21 19:46:13 +0200

    update Furniture

--- Exercise Routing/01.Furniture/src/pages/details.js
+++ Exercise Routing/01.Furniture/src/pages/details.js
@@ -1,7 +1,7 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 import { getItemById, deleteItem } from '../api/data.js';
 
-let detailsTemplate = (item, delItem) => html`
+let detailsTemplate = (item, delItem, userId, ownerId) => html`
 <div class="row space-top">
     <div class="col-md-12">
         <h1>Furniture Details</h1>
@@ -22,10 +22,11 @@ let detailsTemplate = (item, delItem) => html`
         <p>Description: <span>${item.description}</span></p>
         <p>Price: <span>${item.price}</span></p>
         <p>Material: <span>${item.material}</span></p>
+        ${userId == ownerId ? html `
         <div>
             <a href="/edit/${item._id}" class="btn btn-info">Edit</a>
             <a @click=${delItem} href="javascript:void(0)" class="btn btn-red">Delete</a>
-        </div>
+        </div>` : ''}
     </div>
 </div>`;
 
@@ -36,7 +37,11 @@ export async function detailsPage(ctx) {
     // console.log(ctx);
     // console.log(ctx.params.id);
     let item = await getItemById(ctx.params.id);
-    ctx.render(detailsTemplate(item, delItem));
+
+    let userId = sessionStorage.getItem('ownerId')
+    let ownerId = item._ownerId
+
+    ctx.render(detailsTemplate(item, delItem, userId, ownerId));
 
     async function delItem() {
         let confirmation = confirm('Are you sure you want to delete this item?');
