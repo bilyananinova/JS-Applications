1034724dc5abe6331ad53af09a76281ebc16af87
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-03-21 19:27:32 +0200

    update Furniture

--- Exercise Routing/01.Furniture/src/pages/create.js
+++ Exercise Routing/01.Furniture/src/pages/create.js
@@ -1,7 +1,7 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
-import {createItem} from '../api/data.js';
+import { createItem } from '../api/data.js';
 
-let createTemplate = (submit) => html`
+let createTemplate = (submit, invMake, invModel, invYear, invDesc, invPrice, invImg) => html`
 <div class="row space-top">
     <div class="col-md-12">
         <h1>Create New Furniture</h1>
@@ -13,29 +13,29 @@ let createTemplate = (submit) => html`
         <div class="col-md-4">
             <div class="form-group">
                 <label class="form-control-label" for="new-make">Make</label>
-                <input class="form-control valid" id="new-make" type="text" name="make">
+                <input class=${'form-control' + (invMake == true ? ' is-invalid' : '') + (invMake == false ? ' is-valid' : '')} id="new-make" type="text" name="make">
             </div>
             <div class="form-group has-success">
                 <label class="form-control-label" for="new-model">Model</label>
-                <input class="form-control is-valid" id="new-model" type="text" name="model">
+                <input class=${'form-control' + (invModel == true ? ' is-invalid' : '') + (invModel == false ? ' is-valid' : '')} id="new-model" type="text" name="model">
             </div>
             <div class="form-group has-danger">
                 <label class="form-control-label" for="new-year">Year</label>
-                <input class="form-control is-invalid" id="new-year" type="number" name="year">
+                <input class=${'form-control' + (invYear == true ? ' is-invalid' : '') + (invYear == false ? ' is-valid' : '')} id="new-year" type="number" name="year">
             </div>
             <div class="form-group">
                 <label class="form-control-label" for="new-description">Description</label>
-                <input class="form-control" id="new-description" type="text" name="description">
+                <input class=${'form-control' + (invDesc == true ? ' is-invalid' : '') + (invDesc == false ? ' is-valid' : '')} id="new-description" type="text" name="description">
             </div>
         </div>
         <div class="col-md-4">
             <div class="form-group">
                 <label class="form-control-label" for="new-price">Price</label>
-                <input class="form-control" id="new-price" type="number" name="price">
+                <input class=${'form-control' + (invPrice == true ? ' is-invalid' : '') + (invPrice == false ? ' is-valid' : '')} id="new-price" type="number" name="price">
             </div>
             <div class="form-group">
                 <label class="form-control-label" for="new-image">Image</label>
-                <input class="form-control" id="new-image" type="text" name="img">
+                <input class=${'form-control' + (invImg == true ? ' is-invalid' : '') + (invImg == false ? ' is-valid' : '')} id="new-image" type="text" name="img">
             </div>
             <div class="form-group">
                 <label class="form-control-label" for="new-material">Material (optional)</label>
@@ -52,37 +52,60 @@ export async function createPage(ctx) {
 
     async function submit(ev) {
         ev.preventDefault();
+        debugger
         let formData = new FormData(ev.target);
 
         let data = [...formData.entries()].reduce((a, [k, v]) => Object.assign(a, { [k]: v }), {});
 
         if (Object.entries(data).filter(([k, v]) => k != 'material').some(([k, v]) => v == '')) {
+            [...document.querySelectorAll('.form-control')].forEach(i => i.classList.add('is-invalid'))
             return alert('Missing fields!');
         }
 
-        if (formData.get('make').length < 4 || formData.get('model').length < 4) {
-            return alert('Make and Model must be at least 4 symbols long');
+        let invMake = false;
+        let invModel = false;
+        let invYear = false;
+        let invDesc = false;
+        let invPrice = false;
+        let invImg = false;
+
+        if (formData.get('make').length < 4) {
+            invMake = true;
+            alert('Make must be at least 4 symbols long');
+        }
+
+        if (formData.get('model').length < 4) {
+            invModel = true;
+            alert('Model must be at least 4 symbols long');
         }
 
         if (formData.get('year') < 1950 || formData.get('year') > 2050) {
-            return alert('Year must be between 1950 and 2050');
+            invYear = true;
+            alert('Year must be between 1950 and 2050');
         }
 
-        if (formData.get('description').length > 10) {
-            return alert('Description must be more than 10 symbols');
+        if (formData.get('description').length <= 10) {
+            invDesc = true;
+            alert('Description must be more than 10 symbols');
         }
 
         if (formData.get('price') < 0) {
-            return alert('Price must be a positive number');
+            invPrice = true;
+            alert('Price must be a positive number');
         }
 
         if (formData.get('img') == '') {
-            return alert('Image URL is required');
+            invImg = true;
+            alert('Image URL is required');
         }
 
-        data.year = Number(data.year);
-        data.price = Number(data.price);
-        await createItem(data);
-        ctx.page.redirect('/');
+        ctx.render(createTemplate(submit, invMake, invModel, invYear, invDesc, invPrice, invImg));
+
+        if (invMake == false && invModel == false && invYear == false && invDesc == false && invPrice == false && invImg == false) {
+            data.year = Number(data.year);
+            data.price = Number(data.price);
+            await createItem(data);
+            ctx.page.redirect('/');
+        }
     }
 }
