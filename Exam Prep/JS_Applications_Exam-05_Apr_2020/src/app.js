13228e71a4c891c7e4ec23da442a13afb64f497f
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-03-25 19:31:23 +0200

    update SoftWiki

--- Exam Prep/JS_Applications_Exam-05_Apr_2020/src/app.js
+++ Exam Prep/JS_Applications_Exam-05_Apr_2020/src/app.js
@@ -2,17 +2,19 @@ import page from '../node_modules/page/page.mjs';
 import { render } from '../node_modules/lit-html/lit-html.js';
 import { logout } from './api/data.js';
 
-import { homePage } from './views/homePage.js';
-import { createPage } from './views/createPage.js';
-import { registerPage } from './views/registerPage.js';
-import { detailsPage } from './views/detailsPage.js';
-import { editPage } from './views/editPage.js';
+import { loadHome } from './views/home.js';
+import { createPage } from './views/create.js';
+import { registerPage } from './views/register.js';
+import { loginPage } from './views/login.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
 
 let main = document.querySelector('main');
 
-page('/', middleware, homePage);
+page('/', middleware, loadHome);
 page('/create', middleware, createPage);
 page('/register', middleware, registerPage);
+page('/login', middleware, loginPage);
 page('/details/:id', middleware, detailsPage);
 page('/edit/:id', middleware, editPage);
 
@@ -35,9 +37,9 @@ function setUserNav() {
     let token = sessionStorage.getItem('token');
     if (token != null) {
         [...document.querySelectorAll('.user')].forEach(a => a.style.display = 'inline-block');
-        document.querySelector('.guest').style.display = 'none';
+        [...document.querySelectorAll('.guest')].forEach(a => a.style.display = 'none');
     } else {
         [...document.querySelectorAll('.user')].forEach(a => a.style.display = 'none');
-        document.querySelector('.guest').style.display = 'inline-block';
+        [...document.querySelectorAll('.guest')].forEach(a => a.style.display = 'inline-block');
     }
 }
