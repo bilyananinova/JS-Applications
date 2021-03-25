59d8506132b7ba7dd6c2d94201982f9107a121c2
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-03-25 20:37:06 +0200

    update SoftWiki

--- Exam Prep/JS_Applications_Exam-05_Apr_2020/src/views/home.js
+++ Exam Prep/JS_Applications_Exam-05_Apr_2020/src/views/home.js
@@ -1,65 +1,55 @@
-import { html } from 'lit-html';
+import { html } from '../../node_modules/lit-html/lit-html.js';
 import { getItems } from '../api/data.js';
 
-let jsArticles = [];
-let csharpArticles = [];
-let javaArticles = [];
-let pythonArticles = [];
 
-let homeTemplate = (jsArticles, csharpArticles, javaArticles, pythonArticles) => html`
+let homeTemplate = (data) => html`
 <div class="content">
     <section class="js">
         <h2>JavaScript</h2>
         <div class="articles">
-            ${jsArticles.map(article => articleTemp(article))}
+            ${createArticles('JavaScript', data)}
         </div>
     </section>
     <section class="CSharp">
         <h2>C#</h2>
         <div class="articles">
-            ${csharpArticles.map(article => articleTemp(article))}
+            ${createArticles('CSharp', data)}
         </div>
     </section>
     <section class="Java">
         <h2>Java</h2>
         <div class="articles">
-            ${javaArticles.map(article => articleTemp(article))}
+            ${createArticles('Java', data)}
         </div>
     </section>
     <section class="Python">
         <h2>Python</h2>
         <div class="articles">
-            ${pythonArticles ? pythonArticles.map(article => articleTemp(article)) : html `<h3 class="no-articles">No articles yet</h3>`}
+            ${createArticles('Python',data)}
         </div>
     </section>
-</div>`
+    
+</div>`;
 
-let articleTemp = (item) => html`
-<article>
-    <h3>${item.title}</h3>
-    <p>${item.content}</p>
-    ${sessionStorage.getItem('token') 
-    ? html `<a href="/details/${item._id}" class="btn details-btn">Details</a>` 
-    : ''}
-</article>`;
-
+function createArticles(category, data) {
+    let listArticles = data.filter(d => d.category == category)
+    if (listArticles.length == 0) {
+        return html `<h3 class="no-articles">No articles yet</h3>`;
+    } else {
+        return  listArticles.map(d => html`
+        <article>
+                <h3>${d.title}</h3>
+                    <p>${d.content}</p>
+                ${sessionStorage.getItem('token') 
+                ? html `<a href="/details/${d._id}" class="btn details-btn">Details</a>` 
+                : ''}
+        </article>`)
+    }
+}
 
 export async function homePage(ctx) {
     console.log('home');
     let articles = await getItems();
-
-    Object.values(articles).forEach(article => {
-        if (article.category == 'JavaScript') {
-            jsArticles.push(article);
-        } else if (article.category == 'CSharp') {
-            csharpArticles.push(article);
-        } else if (article.category == 'Java') {
-            javaArticles.push(article);
-        } else if (article.category == 'Python') {
-            pythonArticles.push(article);
-        }
-
-    })
-
-    ctx.render(homeTemplate(jsArticles, csharpArticles, javaArticles, pythonArticles))
+    ctx.render(homeTemplate(Object.values(articles)))
+  
 }
